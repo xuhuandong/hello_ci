aca78b1d315c9e2c11c00a724039c527a12265a6
xuhuandong <xuhuandong@126.com>
2019-04-11 11:45:05 +0800

    build failed test

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 int main()
 {
+adfadsf
     Calculator calculator;
     std::cout << "1 + 2 = " << calculator.add(1, 2) << std::endl;
     std::cout << "2 - 1 = " << calculator.sub(2, 1) << std::endl;
