5c7399a2da34f759a1f29a51d68d06a606f5b8ee
xuhuandong <xuhuandong@126.com>
2019-08-23 09:46:28 +0800

    sdfsa

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ int main()
 {
     Calculator calculator;
     std::cout << "1 + 2 = " << calculator.add(1, 2) << std::endl;
-    std::cout << "2 - 1 = " << calculator.sub(2, 1) << std::endl;	
+    std::cout << "2 - 1 = " << calculator.sub(2, 1) << std::endl;
+	
+	
     return 0;
 }
